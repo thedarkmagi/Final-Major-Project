da61858804969e696c3ed0f5cac6c4ea3c45e507
Olly Mills <kinglslbob@gmail.com>
2019-03-03 16:50:05 +0000

    updated documents and resorted file structure

--- Final Major Project - Map Generation/Assets/Scripts/GenerationManager.cs
+++ Final Major Project - Map Generation/Assets/Scripts/GenerationManager.cs
@@ -23,6 +23,8 @@ public class GenerationManager : MonoBehaviour
     GameObject meshObject;
     public Material material;
     Delaunay.Voronoi voronoi;
+    Vector3[] localVerts;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -62,38 +64,144 @@ public class GenerationManager : MonoBehaviour
         voronoi.SiteCoords();
         
         Delaunay.Geo.Polygon polygon = new Polygon(points);
+        //voronoi.Regions()[0][0].
     }
 
-
-    void generateMeshTest()
+    int findVertPosInList(Vector2 vert)
     {
-        Vector3[] localVerts = new Vector3[m_delaunayTriangulation.Count];
-        Vector2[] UVs = new Vector2[m_delaunayTriangulation.Count];
-        for (int i = 0; i < m_delaunayTriangulation.Count; i += 2)
+        for (int i = 0; i < localVerts.Length; i++)
         {
-            Vector2 xy = (Vector2)m_delaunayTriangulation[i].p0;
+            if(vert.x == localVerts[i].x && vert.y == localVerts[i].z)
+            {
+                return i;
+            }
+        }
+            return 0;
+    }
+
+    Vector3[] generateVertList(List<LineSegment> list)
+    {
+        localVerts = new Vector3[list.Count];
+        Vector2[] UVs = new Vector2[list.Count];
+        for (int i = 0; i < list.Count; i+=2)
+        {
+            Vector2 xy = (Vector2)list[i].p0;
             //vertices.Add(new Vector3(xy.x, 0, xy.y));
             localVerts[i] = new Vector3(xy.x, 0, xy.y);
             UVs[i] = xy;
-            xy = (Vector2)m_delaunayTriangulation[i].p1;
+            xy = (Vector2)list[i].p1;
             //vertices.Add(new Vector3(xy.x, 0,xy.y));
             if (i + 1 < localVerts.Length)
             {
                 localVerts[i + 1] = new Vector3(xy.x, 0, xy.y);
-                UVs[i+1] = xy;
+                UVs[i + 1] = xy;
 
             }
         }
-        //Vector3[] localVerts = new Vector3[voronoi.SiteCoords().Count];
+
+        return localVerts;
+    }
+
+
+    Vector2[] generateVertList()
+    {
+        int indexSize=0;
+        for (int i = 0; i < voronoi.Regions().Count; i++)
+        {
+            foreach (var item in voronoi.Regions()[i])
+            {
+                indexSize++;
+                //Debug.Log(indexSize);
+            }
+        } //voronoi.Regions()
+
+        localVerts = new Vector3[indexSize];
+        Vector2[] UVs = new Vector2[indexSize];
+        indexSize = 0;
+        for (int i = 0; i < voronoi.Regions().Count; i++)
+        {
+            foreach (var item in voronoi.Regions()[i])
+            {
+                Vector2 xy = item;
+                Debug.Log(xy);
+                localVerts[indexSize] = new Vector3(xy.x, 0, xy.y);
+                UVs[indexSize] = xy;
+            }
+        }
+
+        
+
+        return UVs;
+    }
+
+    void generateMeshTest()
+    {
+        //Vector3[] localVerts = new Vector3[m_delaunayTriangulation.Count];
+        //Vector2[] UVs = new Vector2[m_delaunayTriangulation.Count];
         //for (int i = 0; i < m_delaunayTriangulation.Count; i += 2)
         //{
         //    Vector2 xy = (Vector2)m_delaunayTriangulation[i].p0;
         //    //vertices.Add(new Vector3(xy.x, 0, xy.y));
         //    localVerts[i] = new Vector3(xy.x, 0, xy.y);
+        //    UVs[i] = xy;
         //    xy = (Vector2)m_delaunayTriangulation[i].p1;
         //    //vertices.Add(new Vector3(xy.x, 0,xy.y));
-        //    localVerts[i + 1] = new Vector3(xy.x, 0, xy.y);
+        //    if (i + 1 < localVerts.Length)
+        //    {
+        //        localVerts[i + 1] = new Vector3(xy.x, 0, xy.y);
+        //        UVs[i+1] = xy;
+
+        //    }
         //}
+        //generateVertList(voronoi.VoronoiDiagram());
+
+        //Vector3[] localVerts = new Vector3[voronoi.VoronoiDiagram().Count];
+        //Vector2[] UVs = new Vector2[voronoi.VoronoiDiagram().Count];
+        //int[] tris = new int[voronoi.VoronoiDiagram().Count];
+        //int triIndex = 0;
+        //for (int i = 0; i < voronoi.VoronoiDiagram().Count; i += 2)
+        //{
+        //    Vector2 xy = (Vector2)voronoi.VoronoiDiagram()[i].p0;
+        //    //vertices.Add(new Vector3(xy.x, 0, xy.y));
+        //    tris[i] = triIndex;
+        //    //voronoi.Region(xy);
+        //    List<Vector2> nextPoint = voronoi.NeighborSitesForSite(xy);
+        //    tris[i+1] = findVertPosInList(nextPoint[0]);
+        //    localVerts[i] = new Vector3(xy.x, 0, xy.y);
+        //    UVs[i] = xy;
+
+
+        //    xy = (Vector2)voronoi.VoronoiDiagram()[i].p1;
+        //    //vertices.Add(new Vector3(xy.x, 0,xy.y));
+        //    if (i + 1 < localVerts.Length)
+        //    {
+        //        nextPoint.Clear();
+        //        //voronoi.Region(xy);
+        //        nextPoint = voronoi.NeighborSitesForSite(xy);
+        //        tris[i + 2] = findVertPosInList(nextPoint[0]);
+        //        localVerts[i + 1] = new Vector3(xy.x, 0, xy.y);
+        //        UVs[i + 1] = xy;
+
+        //    }
+        //}
+
+        //Vector3[] localVerts = new Vector3[voronoi.SiteCoords().Count];
+        //Vector2[] UVs = new Vector2[voronoi.SiteCoords().Count];
+        //for (int i = 0; i < voronoi.SiteCoords().Count; i ++)
+        //{
+        //    Vector2 xy = voronoi.SiteCoords()[i]; 
+        //    //vertices.Add(new Vector3(xy.x, 0, xy.y));
+        //    localVerts[i] = new Vector3(xy.x, 0, xy.y);
+        //    UVs[i] = xy;
+        //    //UVs[i] = new Vector2(xy.x / 100.0f, xy.y / 100.0f);
+        //    //xy = voronoi.SiteCoords()[i+1];
+        //    ////vertices.Add(new Vector3(xy.x, 0,xy.y));
+        //    //localVerts[i + 1] = new Vector3(xy.x, 0, xy.y);
+        //}
+
+
+
+        Vector2[] UVs = generateVertList();
 
         meshObject = new GameObject("TerrainChunk");
         meshRenderer = meshObject.AddComponent<MeshRenderer>();
@@ -103,31 +211,47 @@ public class GenerationManager : MonoBehaviour
 
         Mesh mesh = new Mesh();
 
-        mesh.vertices = localVerts; 
-        mesh.triangles = convertTriTointArray(voronoi.GetSites());
-
+        mesh.vertices = localVerts;
+        //mesh.triangles = convertTriTointArray(voronoi.GetSites());
+        mesh.triangles = triArray();
         mesh.uv = UVs;// FlatShading(mesh.triangles);
 
         meshFilter.mesh = mesh;
     }
+    int[] triArray()
+    {
+        int[] triArray = new int[localVerts.Length*3];
+        int triangleIndex = 0;
+        foreach (var item in localVerts)
+        {
+            triArray[triangleIndex] = triangleIndex;
+            triangleIndex++;
+        }
 
+        return triArray;
+
+    }
 
     int[] convertTriTointArray(SiteList triList)
     {
         //triList.Next()
         int[] triArray = new int[ triList.Count*3];
         int currentArrayIndex=0;
+        int triangleIndex=0;
         for (int i = 0; i < triList.Count; i+=3)
         {
-            
-                // need a way to get a position in array for this site  as X is just fundimenally incorrect 
-                triArray[currentArrayIndex] = (int)triList.SiteCoords()[i].x;
+
+            // need a way to get a position in array for this site  as X is just fundimenally incorrect 
+            triArray[currentArrayIndex] = triangleIndex;//(int)triList.SiteCoords()[i].x;
+            triangleIndex++;
             if (triList.SiteCoords().Count > i + 1)
             {
-                triArray[currentArrayIndex + 1] = (int)triList.SiteCoords()[i + 1].x;
+                triArray[currentArrayIndex + 1] = triangleIndex;//(int)triList.SiteCoords()[i + 1].x;
+                triangleIndex++;
                 if (triList.SiteCoords().Count > i + 2)
                 {
-                    triArray[currentArrayIndex + 2] = (int)triList.SiteCoords()[i + 2].x;
+                    triArray[currentArrayIndex + 2] = triangleIndex;//(int)triList.SiteCoords()[i + 2].x;
+                    triangleIndex++;
                 }
             }
                 currentArrayIndex += 3;
