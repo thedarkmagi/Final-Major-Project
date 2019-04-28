155c51dd7248f0fc33c464e3eaf14d9c9e793ca6
Olly Mills <kinglslbob@gmail.com>
2019-04-28 15:53:43 +0100

    now correctly identifying the border of the the islands verts

--- Final Major Project - Map Generation/Assets/Scripts/VoronoiGeneration.cs
+++ Final Major Project - Map Generation/Assets/Scripts/VoronoiGeneration.cs
@@ -6,7 +6,7 @@ using TriangleNet.Topology;
 
 public class VoronoiGeneration : MonoBehaviour
 {
-    private const float islandHeight = 0.9f;
+    private const float islandHeight = 0.5f;
     TriangleNet.Mesh mesh;
     TriangleNet.Mesh shrunkMesh;
     Polygon polygon;
@@ -89,7 +89,7 @@ public class VoronoiGeneration : MonoBehaviour
 
             elevations.Add(sample);
         }
-        print("Max X: " + maxX + " Max Y: " + maxY);
+        //print("Max X: " + maxX + " Max Y: " + maxY);
         //findMountainPeak(mesh.vertices);
 
         Renderer textureRenderer = chunkPrefab.GetComponent<Renderer>();
@@ -260,7 +260,8 @@ public class VoronoiGeneration : MonoBehaviour
 
             // triangles
             List<int> triangles = new List<int>();
-
+            // triangles custom 
+            List<List<int>> trisList = new List<List<int>>();
             //vertex colours
             List<Color> vertColors = new List<Color>();
             //Bounds bounds = new Bounds(new Vector3((float)mesh.Bounds.Left - (float)mesh.Bounds.Right, (float)mesh.Bounds.Top - (float)mesh.Bounds.Bottom), new Vector3((float)mesh.Bounds.Width, (float)mesh.Bounds.Height));
@@ -293,9 +294,13 @@ public class VoronoiGeneration : MonoBehaviour
                 vertColors.Add(getVertColour(v0));
                 vertColors.Add(getVertColour(v1));
                 vertColors.Add(getVertColour(v2));
-
-                //this will need more work, need to get mesh vert form triangle.id to be passed in
-                //vertColors.Add(getBiomeType(triangle.vertices[2])); 
+                List<int> thisTri = new List<int>();
+                thisTri.Add(vertices.Count);
+                thisTri.Add(vertices.Count + 1);
+                thisTri.Add(vertices.Count + 2);
+                trisList.Add(thisTri);
+                //this will need more work,vertices.Count + 1 need to get mesh vert form triangle.id to be passed in
+                //vertColors.Add(getBiomeTyvertices.Count + 2pe(triangle.vertices[2])); 
                 //vertColors.Add(getVertColour(v1));
                 //vertColors.Add(getVertColour(v2));
 
@@ -322,9 +327,12 @@ public class VoronoiGeneration : MonoBehaviour
 
             Dictionary<int, BiomeType> vertBiomes = new Dictionary<int, BiomeType>();
             VertexConnection[] vertCons = findVertConnections(chunkMesh);
-            vertBiomes = setVertBiomes(vertCons, chunkMesh);
-            List<int> borderVerts = findBorderVerts(vertCons, vertBiomes, chunkMesh, BiomeType.land, BiomeType.water);
-            printList(borderVerts);
+            //printBiomeDictionary(vertBiomes);
+            //print(vertCons.Length);
+            vertBiomes = setVertBiomes( chunkMesh);
+            //printBiomeDictionary(vertBiomes);
+            List<int> borderVerts = findBorderVerts(trisList, vertBiomes, chunkMesh, BiomeType.land, BiomeType.water);
+            //printList(borderVerts);
             //GameObject chunk = Instantiate<GameObject>(chunkPrefab, transform.position, transform.rotation);
             GameObject chunk = Instantiate(chunkPrefab, new Vector3(transform.position.x + xOffSet, transform.position.y, transform.position.z + yOffSet), transform.rotation);
             chunk.GetComponent<MeshFilter>().mesh = chunkMesh;
@@ -336,7 +344,14 @@ public class VoronoiGeneration : MonoBehaviour
     {
         for (int i = 0; i < list.Count; i++)
         {
-            print(list[i]);
+            print(list[i] + "BorderVerts");
+        }
+    }
+    void printBiomeDictionary(Dictionary<int,BiomeType> dictionary)
+    {
+        foreach (var item in dictionary)
+        {
+            print(item.Value);
         }
     }
     /* Returns a point's local coordinates. */
@@ -388,6 +403,7 @@ public class VoronoiGeneration : MonoBehaviour
     VertexConnection[] findVertConnections(Mesh mesh)
     {
         Vector3[] vertices = mesh.vertices;
+        
         VertexConnection[] connections = new VertexConnection[vertices.Length];
 
         for (int i = 0; i < vertices.Length; i++)
@@ -410,21 +426,33 @@ public class VoronoiGeneration : MonoBehaviour
         }
         return connections;
     }
-    Dictionary<int, BiomeType> setVertBiomes(VertexConnection[] vertCons, Mesh mesh)
+
+    bool findVertIndexOfAdjenctVerts(Mesh mesh,int vertIndex, List<List<int>> triList, Dictionary<int, BiomeType> vertBiomes, BiomeType targetBiome, BiomeType secondTargetBiome)
     {
-        Dictionary<int, BiomeType> result = new Dictionary<int, BiomeType>();
-        for (int i = 0; i < vertCons.Length; i++)
+        List<int> adjecentVertIndexs = new List<int>();
+        for (int i = 0; i < triList.Count; i++)
         {
-            if (vertCons[i] != null)
+            for (int j = 0; j < triList[i].Count; j++)
             {
-                if (vertCons[i].connections != null)
+                if(triList[i].Contains(vertIndex))
                 {
-                    for (int j = 0; j < vertCons[i].connections.Count; j++)
+                    if(triList[i][j]!=vertIndex)
                     {
-                        result[i] = setBiomeType(mesh.vertices[vertCons[i].connections[j]].y);
+                        adjecentVertIndexs.Add(triList[i][j]);
                     }
                 }
             }
+        }
+        return findTransitionVerts(adjecentVertIndexs, vertBiomes, vertIndex, targetBiome, secondTargetBiome);
+    }
+    Dictionary<int, BiomeType> setVertBiomes(Mesh mesh)
+    {
+        print("we are setting biomes");
+        Dictionary<int, BiomeType> result = new Dictionary<int, BiomeType>();
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            result[i] = setBiomeType(mesh.vertices[i].y);
+
         }
         return result;
     }
@@ -453,7 +481,49 @@ public class VoronoiGeneration : MonoBehaviour
         }
         return result;
     }
-    //function currently never results in true. 
+    bool findTransitionVerts(List<int> adjecentVertIndexs, Dictionary<int, BiomeType> vertBiomes, int vertIndex, BiomeType targetBiome, BiomeType secondTargetBiome)
+    {
+        bool result = false;
+        bool biomeOne = false;
+        bool biomeTwo = false;
+
+
+        for (int i = 0; i < adjecentVertIndexs.Count; i++)
+        {
+            if (vertBiomes[vertIndex] == targetBiome)
+            {
+                biomeOne = true;
+                //print("biomes do actually matching at least once biome 1");
+                BiomeType temp = vertBiomes[adjecentVertIndexs[i]];
+                //print("tempCheck " + temp + " secondBiome target " + secondTargetBiome);
+                if (temp == secondTargetBiome)
+                {
+                    biomeTwo = true;
+                }
+            }
+            if (vertBiomes[vertIndex] == secondTargetBiome)
+            {
+                biomeTwo = true;
+                //print("biomes do actually matching at least once biome 2");
+                BiomeType temp = vertBiomes[adjecentVertIndexs[i]];
+                //print("tempCheck " + temp + " FirstBiome target " + targetBiome);
+                if (temp == targetBiome)
+                {
+                    biomeOne = true;
+                }
+            }
+        
+            if (biomeOne && biomeTwo)
+            {
+                //print("TransitionFound <3");
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
+    //function currently never results in true. It's cursed as hell just like everything relating 
     bool findTransitionVerts(VertexConnection[] vertCons, Dictionary<int, BiomeType> vertBiomes, int vertIndex, BiomeType targetBiome, BiomeType secondTargetBiome)
     {
         bool result = false;
@@ -469,49 +539,62 @@ public class VoronoiGeneration : MonoBehaviour
                     if (vertBiomes[vertIndex] == targetBiome)
                     {
                         biomeOne = true;
-                        if (vertBiomes[vertCons[vertIndex].connections[i]] == secondTargetBiome)
+                        //print("biomes do actually matching at least once biome 1");
+                        BiomeType temp = vertBiomes[vertCons[vertIndex].connections[i]];
+                        print("tempCheck " + temp + " secondBiome target "+secondTargetBiome);
+                        if (temp == secondTargetBiome)
                         {
                             biomeTwo = true;
+                            print("found a match");
                         }
                     }
-                    else if (vertBiomes[vertIndex] == secondTargetBiome)
+                    if (vertBiomes[vertIndex] == secondTargetBiome)
                     {
                         biomeTwo = true;
-                        if (vertBiomes[vertCons[vertIndex].connections[i]] == targetBiome)
+                        //print("biomes do actually matching at least once biome 2");
+                        BiomeType temp = vertBiomes[vertCons[vertIndex].connections[i]];
+                        print("tempCheck " + temp + " FirstBiome target " + targetBiome);
+                        if (temp == targetBiome)
                         {
                             biomeOne = true;
+                            print("found a match2");
                         }
                     }
 
 
-                    //if (vertBiomes[vertCons[vertIndex].connections[i]] == targetBiome)
-                    //{
-                    //    biomeOne = true;
-                    //}
-                    //else if (vertBiomes[vertCons[vertIndex].connections[i]] == secondTargetBiome)
-                    //{
-                    //    biomeTwo = true;
-                    //}
+                    if (vertBiomes[vertCons[vertIndex].connections[i]] == targetBiome)
+                    {
+                        biomeOne = true;
+                    }
+                    if (vertBiomes[vertCons[vertIndex].connections[i]] == secondTargetBiome)
+                    {
+                        biomeTwo = true;
+                    }
 
                     if (biomeOne && biomeTwo)
                     {
+                        print("TransitionFound <3");
                         result = true;
                         break;
                     }
                 }
             }
         }
+        if (biomeOne && biomeTwo)
+        {
+            print("TransitionFound <3 but somewhere weird");
+            result = true;
+        }
         return result;
     }
-    List<int> findBorderVerts(VertexConnection[] vertCons, Dictionary<int, BiomeType> vertBiomes, Mesh mesh, BiomeType targetBiome, BiomeType secondTargetBiome)
+    List<int> findBorderVerts(List<List<int>> triList, Dictionary<int, BiomeType> vertBiomes, Mesh mesh, BiomeType targetBiome, BiomeType secondTargetBiome)
     {
         List<int> borderVerts = new List<int>();
         for (int i = 0; i < mesh.vertices.Length; i++)
         {
-
-            if (findTransitionVerts(vertCons, vertBiomes, i, targetBiome, secondTargetBiome))
+            if (findVertIndexOfAdjenctVerts(mesh,i,triList, vertBiomes, targetBiome, secondTargetBiome))
             {
-                print("inside the loop " + i);
+                //print("inside the loop " + i);
                 borderVerts.Add(i);
             }
         }
