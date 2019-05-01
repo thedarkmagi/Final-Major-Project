b25c639057ff0eb33e9583e6ccfa4449eca66b39
Olly Mills <kinglslbob@gmail.com>
2019-05-01 13:31:50 +0100

    can now set the icon to MVP token

--- Final Major Project - Map Generation/Assets/Scripts/TokenController.cs
+++ Final Major Project - Map Generation/Assets/Scripts/TokenController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TokenController : MonoBehaviour
 {
     public List<GameObject> uiButtons;
     public MouseInput mouseInputReference;
+    public GameObject iconPanel;
+    public GameObject IconPrefab;
+    public Image Icon;
+    public List<Sprite> IconList;
+
+    private List<GameObject> currentIconsInPanel = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
         setButtionsState(false);
+        iconPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -38,6 +46,27 @@ public class TokenController : MonoBehaviour
         mouseInputReference.setMouseState(MouseInput.MouseState.moveMVP);
         mouseInputReference.setSelectedToken(gameObject);
         DeactiveButtons();
+    }
+    public void enableEditMode()
+    {
+        DeactiveButtons();
+        iconPanel.SetActive(true);
+        for (int i = 0; i < IconList.Count; i++)
+        {
+            GameObject tempIcon = Instantiate(IconPrefab, iconPanel.transform);
+            tempIcon.GetComponent<Image>().sprite = IconList[i];
+            currentIconsInPanel.Add(tempIcon);
+        }
+    }
+    public void SetIcon(Image newImage)
+    {
+        Icon.sprite = newImage.sprite;
+        for (int i = 0; i < currentIconsInPanel.Count; i++)
+        {
+            Destroy(currentIconsInPanel[i]);
+        }
+        currentIconsInPanel.Clear();
+        iconPanel.SetActive(false);
     }
     public void killToken()
     {
